0fee1943435c82960323b2f009ac18d5b0533728
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-10-06 04:20:17 +0300

    added functions

--- src/services/near.js
+++ src/services/near.js
@@ -12,3 +12,70 @@ export const near = new Near({
   });
 
 export const wallet = new WalletConnection(near, "communite");
+
+export const getComplaints = () => {
+  return wallet.account().viewFunction(CONTRACT_ID, "getComplaints")
+}
+
+// will return the metadata of the complaints
+export const getNComplaints = () => {
+  return wallet.account().viewFunction(CONTRACT_ID, "getNComplaints")
+}
+
+// returns the amount of tickets that i've issued
+export const getNumberOfComplaints = () => {
+  return wallet.account().viewFunction(CONTRACT_ID, "getNumberOfComplaints")
+}
+
+
+
+//function to sendMessage
+export const addNewComplaint = ({title, description, category, location}) => {
+  console.log(title, description, category, location)
+  return wallet.account().functionCall({
+      contractId: CONTRACT_ID,
+      methodName: "addNewComplaint",
+      gas,
+      args: {title, description, category, location}
+  })
+}
+
+export const voteComplaint = ({id}) => {
+  console.log(id)
+  return wallet.account().functionCall({
+      contractId: CONTRACT_ID,
+      methodName: "voteComplaint",
+      gas,
+      args: {id}
+  })
+}
+
+export const removeVote = ({id}) => {
+  console.log(id)
+  return wallet.account().functionCall({
+      contractId: CONTRACT_ID,
+      methodName: "removeVote",
+      gas,
+      args: {id}
+  })
+}
+
+export const takeComplaint = ({id}) => {
+  console.log(id)
+  return wallet.account().functionCall({
+      contractId: CONTRACT_ID,
+      methodName: "takeComplaint",
+      gas,
+      args: {id}
+  })
+}
+
+export const finishComplaint = ({id}) => {
+  console.log(id)
+  return wallet.account().functionCall({
+      contractId: CONTRACT_ID,
+      methodName: "finishComplaint",
+      gas,
+      args: {id}
+  })
+}
